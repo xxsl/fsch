82e1c94298c8bf5250091faa38d396ceba30de1c
nimrod97 <nimrod97@eab46b8c-84ff-11dd-90a9-6306d3ffb32f>
2008-09-20 02:38:31 +0000

    dev

--- build/relink.bat
+++ build/relink.bat
@@ -1,2 +1,7 @@
 rem relink to use stdout
+del server.exe
+del client.exe
+copy fcshWrapper.exe server.exe /y
 LINK.EXE /EDIT /SUBSYSTEM:CONSOLE c:\work\trunk\build\fcshWrapper.exe
+copy fcshWrapper.exe client.exe /y
+del fcshWrapper.exe
