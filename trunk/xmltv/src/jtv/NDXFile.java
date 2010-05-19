b92fce798b3fdd32b4984306241cb0886757a2c1
nimrod97 <nimrod97@eab46b8c-84ff-11dd-90a9-6306d3ffb32f>
2010-05-19 12:52:55 +0000

    example

--- trunk/xmltv/src/jtv/NDXFile.java
+++ trunk/xmltv/src/jtv/NDXFile.java
@@ -23,29 +23,26 @@ public class NDXFile
     public Long read() throws IOException
     {
         ndxTimes = new ArrayList<NDXTime>();
-        DataInputStream in = null;
+        LEDataInputStream in = null;
         try
         {
-            in = new DataInputStream(new BufferedInputStream(new FileInputStream(file)));
+            in = new LEDataInputStream(new BufferedInputStream(new FileInputStream(file)));
             //first 2 bytes is number of records
-            size = Short.reverseBytes(in.readShort());
+            size = in.readShort();
+
             for (long i = 0; i < size; i++)
             {
-                //in.skipBytes(12);
                 //2 zero bytes
                 in.skipBytes(2);
-                //      : dwHighDateTime : 30076942 : Long : converter.shiftFile
-                //      : dwLowDateTime : 1746311168 : Long : converter.shiftFile
-                long time = Long.reverseBytes(in.readLong());
-                long offset = Short.reverseBytes(in.readShort());
+                long time = FileTimes.getJavaTime(in.readLong());
+                long offset = in.readShort();
                 NDXTime ndxTime = new NDXTime(offset, time);
                 ndxTimes.add(ndxTime);
             }
-
         }
         finally
         {
-            if (in != null)
+            if(in != null)
             {
                 in.close();
             }
