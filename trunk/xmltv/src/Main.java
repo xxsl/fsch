4fd1c9e2845c66b02e3850c0aeaf97436fb812af
nimrod97 <nimrod97@eab46b8c-84ff-11dd-90a9-6306d3ffb32f>
2010-05-20 16:42:11 +0000

    impl

--- trunk/xmltv/src/Main.java
+++ trunk/xmltv/src/Main.java
@@ -29,7 +29,7 @@ public class Main
 
         // unmarshal from foo.xml
         Unmarshaller u = jc.createUnmarshaller();
-        Tv tv = (Tv) u.unmarshal(new File("J:\\Projects\\fsch\\xmltv\\dtd\\program.xml"));
+        Tv tv = (Tv) u.unmarshal(new File("I:\\work\\xmltv\\dtd\\program_xml.xml"));
 
 
         XMLTV2JTV xmltv2JTV = new XMLTV2JTV(tv);
@@ -47,7 +47,7 @@ public class Main
                 jProgramme.setStart(new Date((jProgramme.getStart().getTime() - 1000 * 60 * 60)));
             }
 
-            JFileChannel jFileChannel = new JFileChannel(new File("J:\\Projects\\fsch\\xmltv\\jtv\\program_jtv1"), jChannel, "Cp1251");
+            JFileChannel jFileChannel = new JFileChannel(new File("I:\\work\\xmltv\\jtv\\program_jtv1"), jChannel, "Cp1251");
             jFileChannel.write();
         }
 
